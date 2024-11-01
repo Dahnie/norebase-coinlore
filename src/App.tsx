f793f3c3b20a94b7d462862809f1936ed93cb167
Daniel Adeneye <adeneyedaniel007@gmail.com>
2024-11-01 23:31:53 +0100

    removed netlify toml file

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <ToastHandlerContextProvider>
       <Provider store={store}>
-        <Router>
+        <Router basename={"/norebase-coinlore"}>
           <AppRoutes />
         </Router>
       </Provider>
